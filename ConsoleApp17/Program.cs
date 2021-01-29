82b5738d296cb1972e7e44fa37a3959456884dac
User <User@DEXY8>
2021-01-29 20:28:09 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        interface IRunnable
+        {
+            int A { get; set; }
+            int this[int i] { get; set; }
+        }
+        class Runnable : IRunnable
+        {
+            public int this [int i] { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            ReadKey();
+        }
+    }
+}
